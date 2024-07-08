a153df630b64224594dfd44d9b2b19e4b4a37c5b
= <=>
2024-07-08 01:34:59 +0000

    packages

--- dbackend/users/models.py
+++ dbackend/users/models.py
@@ -37,7 +37,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['fullname']
+    REQUIRED_FIELDS = ['fullname', 'telephone']
 
     def __str__(self):
         return str(self.email)
